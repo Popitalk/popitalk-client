a3cf42d2b59d2c71457309c6c823e2ada4c146ae
SulaimanAlabbar <sulaimannajeebalabbar@gmail.com>
2019-12-10 13:28:11 +0300

    fixed popup bug

--- src/components/HeaderNotifications/HeaderNotifications.jsx
+++ src/components/HeaderNotifications/HeaderNotifications.jsx
@@ -4,7 +4,7 @@ import Notifications from "../Notifications";
 import "./HeaderNotifications.css";
 
 function HeaderNotifications() {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const toggle = () => setOpen(!open);
   HeaderNotifications.handleClickOutside = () => setOpen(false);
 
