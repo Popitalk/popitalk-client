69bbb2e05ea70ae24c42d939f1f9af52499ad3a5
Michael-Xie <xiemicha@gmail.com>
2020-05-11 02:24:26 -0400

    Update AvatarIcon for more customization with tailwind

--- src/comp/InfoCards/AvatarIcon.jsx
+++ src/comp/InfoCards/AvatarIcon.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
-export default function AvatarIcon({ avatar, username, imageClick }) {
+export default function AvatarIcon({
+  avatar,
+  username,
+  imageClick,
+  className = ""
+}) {
   const handleClick = event => {
     event.stopPropagation();
     imageClick();
   };
 
+  const defaultClassName = "img w-12 h-12 rounded-circle";
   return (
     <img
-      className="img w-12 h-12 rounded-circle"
+      className={className ? className : defaultClassName}
       role="button"
       src={avatar}
       alt={`${username}'s avatar`}
---
+++ src/comp/VideoSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./VideoStatus.css";
+import RoomIcon from "./RoomIcon";
+
+export default function VideoSection({ id, name, icon, videoStatus }) {
+  return (
+    <div>
+      <div>
+        <p>Video Container for Video Player</p>
+        <div></div>
+      </div>
+      <section></section>
+    </div>
+  );
+}
