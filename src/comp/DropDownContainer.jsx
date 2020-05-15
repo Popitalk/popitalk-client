4fb09abc29070ca29f2f88963235e015d2490690
Emily Palmieri <emily.palmieri@hotmail.com>
2020-05-14 20:17:25 -0700

    -Added Anonymous Sidebar -Created new Story for Channel Settings

--- src/comp/DropDownContainer.jsx
+++ src/comp/DropDownContainer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import classnames from "classnames";
 
 export default function DropDownContainer({ children }) {
-  const modalClasses = classnames(
+  const classes = classnames(
     "w-full sm:w-dropdown object-center bg-primaryBackground rounded-xl shadow-xl border border-highlightText"
   );
 
-  return <div className={modalClasses}>{children}</div>;
+  return <div className={classes}>{children}</div>;
 }
