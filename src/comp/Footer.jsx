f3aa800d297b8c1eba6f1c2e122051839a9b6d28
SKaralius <k.simonas.k@gmail.com>
2020-05-24 11:34:18 +0300

    Footer extracted to seperate component. Footer responsive.

---
+++ src/comp/Footer.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="absolute inset-x-0 px-4 py-8 -mt-4 bg-gradient-r-primary text-tertiaryText sm:px-16">
+      <div className="grid grid-cols-2 row-gap-5 sm:flex sm:justify-start sm:space-x-16 sm:ml-10 sm:mt-5">
+        <div className="space-y-2 text-center">
+          <span className="mb-4 text-lg font-bold">Company</span>
+          <ul className="space-y-2 text-sm">
+            <li>
+              <p>About Popitalk</p>
+            </li>
+            <li>
+              <p>Jobs</p>
+            </li>
+          </ul>
+        </div>
+        <div className="space-y-2 text-center">
+          <span className="mb-4 text-lg font-bold">Legal</span>
+          <ul className="space-y-2 text-sm">
+            <li>
+              <p>Terms of Use</p>
+            </li>
+            <li>
+              <p>Privacy Policy</p>
+            </li>
+            <li>
+              <p>Copyright</p>
+            </li>
+          </ul>
+        </div>
+        <div className="space-y-2 text-center">
+          <span className="mb-4 text-lg font-bold">Community</span>
+          <ul className="space-y-2 text-sm">
+            <li>
+              <p>Guidelines</p>
+            </li>
+            <li>
+              <p>Support</p>
+            </li>
+          </ul>
+        </div>
+        <div className="space-y-2 text-center">
+          <span className="mb-4 text-lg font-bold">Business</span>
+          <ul className="space-y-2 text-sm">
+            <li>
+              <p>Branding</p>
+            </li>
+            <li>
+              <p>Contact</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex justify-end mt-8">
+        <p>© 2020 Popitalk, Inc. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/comp/WelcomePage.jsx
+++ src/comp/WelcomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SiteHeaderWelcome from "./SiteHeaderWelcome";
 import CreateNewAccountForm from "./CreateNewAccountForm";
-
+import Footer from "./Footer";
 import Welcome from "../assets/welcome.png";
 
 function WelcomePage() {
@@ -17,60 +17,7 @@ function WelcomePage() {
             <CreateNewAccountForm />
           </div>
         </section>
-        <footer className="absolute inset-x-0 px-16 py-8 -mt-4 bg-gradient-r-primary text-tertiaryText">
-          <div className="flex space-x-8">
-            <div className="space-y-2 text-center">
-              <span className="mb-4 text-lg font-bold">Company</span>
-              <ul className="space-y-2 text-sm">
-                <li>
-                  <p>About Popitalk</p>
-                </li>
-                <li>
-                  <p>Jobs</p>
-                </li>
-              </ul>
-            </div>
-            <div className="space-y-2 text-center">
-              <span className="mb-4 text-lg font-bold">Legal</span>
-              <ul className="space-y-2 text-sm">
-                <li>
-                  <p>Terms of Use</p>
-                </li>
-                <li>
-                  <p>Privacy Policy</p>
-                </li>
-                <li>
-                  <p>Copyright</p>
-                </li>
-              </ul>
-            </div>
-            <div className="space-y-2 text-center">
-              <span className="mb-4 text-lg font-bold">Community</span>
-              <ul className="space-y-2 text-sm">
-                <li>
-                  <p>Guidelines</p>
-                </li>
-                <li>
-                  <p>Support</p>
-                </li>
-              </ul>
-            </div>
-            <div className="space-y-2 text-center">
-              <span className="mb-4 text-lg font-bold">Business</span>
-              <ul className="space-y-2 text-sm">
-                <li>
-                  <p>Branding</p>
-                </li>
-                <li>
-                  <p>Contact</p>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="flex justify-end mt-8">
-            <p>© 2020 Popitalk, Inc. All rights reserved.</p>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </>
   );
