bb0d0252b594453273531a23a2c69765e90065a3
Michael-Xie <xiemicha@gmail.com>
2020-05-24 02:11:05 -0400

    Add onClick prop to Input component

--- src/comp/Input.jsx
+++ src/comp/Input.jsx
@@ -47,6 +47,7 @@ export default function Input({
   interiorButton,
   forwardedRef,
   className,
+  onClick = () => console.log("clicked"),
   ...rest
 }) {
   const El = variant === "textarea" ? "textarea" : "input";
@@ -116,6 +117,7 @@ export default function Input({
               size="sm"
               background="secondary"
               className="absolute right-0 mr-4"
+              onClick={onClick}
             />
           </>
         )}
