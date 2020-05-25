0e1e138091076f6dd3988cccd7588e6f18c52941
Michael-Xie <xiemicha@gmail.com>
2020-05-25 00:18:28 -0400

    Update VideoChannelHeader to have the section highlight dynamic onClick

--- src/comp/VideoChannelHeader.jsx
+++ src/comp/VideoChannelHeader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "./Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./VideoStatus.css";
@@ -9,8 +9,11 @@ export default function VideoChannelHeader({
   name,
   icon,
   videoStatus,
-  type = "channel"
+  type = "channel",
+  select = "Video"
 }) {
+  const [selected, setSelected] = useState(select);
+  const navButtons = ["Video", "Queue", "Channel", "Setting"]
   return (
     <header className="flex justify-between bg-disabledBackground my-1 mx-1">
       <div className="flex items-center">
@@ -26,30 +29,18 @@ export default function VideoChannelHeader({
         </p>
       </div>
       <nav className="flex flex-wrap justify-center">
-        <a
-          href="https://www.google.com"
-          className="btn-playing mx-1 my-1 font-bold no-underline"
-        >
-          Video
-        </a>
-        <a
-          href="https://www.google.com"
-          className="mx-1 my-1 font-bold text-secondaryText no-underline"
-        >
-          Queue
-        </a>
-        <a
-          href="https://www.google.com"
-          className="mx-1 my-1 font-bold text-secondaryText no-underline"
-        >
-          Channel
-        </a>
-        <a
-          href="https://www.google.com"
-          className="mx-1 my-1 font-bold text-secondaryText no-underline"
-        >
-          Settings
-        </a>
+        {navButtons.map((buttonName) => {
+          let className = "mx-1 my-1 font-bold focus:outline-none";
+          if (selected == buttonName) {
+            className = `${className} rainbow-text`
+          } else {
+            className = `${className} text-secondaryText`
+          }
+          return (
+            <button className={className} onClick={()=> setSelected(buttonName)}>{buttonName}</button>
+          )
+        })}
+
       </nav>
     </header>
   );
