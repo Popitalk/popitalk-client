0e13e5a6809bb0616f73d38fcaf216de16c8ed3e
Michael-Xie <xiemicha@gmail.com>
2020-06-02 21:44:16 -0400

    Update ChannelHeader to use Link

--- src/comp/ChannelHeader.jsx
+++ src/comp/ChannelHeader.jsx
@@ -1,6 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useLayoutEffect, useRef } from "react";
 import RoomIcon from "./RoomIcon";
+import { useSelector, useDispatch } from "react-redux";
 
+import {
+  Link,
+  Switch,
+  Route,
+  useRouteMatch,
+  useLocation,
+  useHistory,
+  useParams
+} from "react-router-dom";
 export default function ChannelHeader({
   id,
   name,
@@ -10,7 +20,38 @@ export default function ChannelHeader({
   select = "Video"
 }) {
   const [selected, setSelected] = useState(select);
-  const navButtons = ["Video", "Queue", "Channel", "Setting"];
+  const navButtons = [
+    { name: "Video", endpoint: "video" },
+    { name: "Queue", endpoint: "queue" },
+    { name: "Channel", endpoint: "channel" },
+    { name: "Setting", endpoint: "setting" }
+  ];
+
+  const channel = useSelector(state => state.channels[id]);
+
+  const match = useRouteMatch();
+  const location = useLocation();
+  const channelRef = useRef(null);
+  const scrollRef = useRef(null);
+  // const { id: ownId } = useSelector(state => state.self);
+  const loading = useSelector(state => !state.channels[id]?.loaded);
+
+  console.log(
+    "match",
+    match,
+    "location",
+    location,
+    "channelRef",
+    channelRef,
+    "scrollRef",
+    scrollRef,
+    // "id",
+    // ownId,
+    "loading",
+    loading,
+    "channel",
+    channel
+  );
   return (
     <header className="flex justify-between bg-disabledBackground my-1 mx-1">
       <div className="flex items-center">
@@ -27,20 +68,22 @@ export default function ChannelHeader({
       </div>
       {type === "channel" && (
         <nav className="flex flex-wrap justify-center">
-          {navButtons.map(buttonName => {
+          {navButtons.map((button, idx) => {
             let className = "mx-1 my-1 font-bold focus:outline-none";
-            if (selected == buttonName) {
+            if (selected == button.name) {
               className = `${className} rainbow-text`;
             } else {
               className = `${className} text-secondaryText`;
             }
             return (
-              <button
-                className={className}
-                onClick={() => setSelected(buttonName)}
-              >
-                {buttonName}
-              </button>
+              <Link to={`${match.url}/${button.endpoint}`} key={idx}>
+                <button
+                  className={className}
+                  onClick={() => setSelected(button.name)}
+                >
+                  {button.name}
+                </button>
+              </Link>
             );
           })}
         </nav>
