67538f329169e6814e7b0400d88b1ac528e53a44
SKaralius <k.simonas.k@gmail.com>
2020-06-07 14:42:52 +0300

    Makes LeftPanel show on all pages except for welcome and profile.
    When user is in MainPage, channel swithces page, but when you are in a room, the panel doesn't change page, only itself.

--- src/containers/LeftPanel.jsx
+++ src/containers/LeftPanel.jsx
@@ -7,12 +7,13 @@ import history from "../history";
 
 export default function LeftPanelContainer() {
   const [selectedChannel, setSelectedChannel] = useState(null);
+  const [selectedPage, setSelectedPage] = useState("channels");
   const channels = useSelector(state => state.channels);
   const friends = useSelector(state => state.relationships.friends);
   const isCollapsed = useSelector(state => state.ui.isCollapsed);
   const dispatch = useDispatch();
 
-  const updateSelectedPage = page => {
+  const updateSelectedPageAndMain = page => {
     const pages = {
       channels: "channels",
       friends: "friends"
@@ -23,6 +24,19 @@ export default function LeftPanelContainer() {
       console.log("no such page exists.");
     }
   };
+
+  const updateSelectedPanelPage = page => {
+    const pages = {
+      channels: "channels",
+      friends: "friends"
+    };
+    if (pages[page]) {
+      setSelectedPage(pages[page]);
+    } else {
+      console.log("no such page exists.");
+    }
+  };
+
   return (
     <Switch>
       <Route exact path="/channels">
@@ -31,7 +45,7 @@ export default function LeftPanelContainer() {
           friends={friends}
           selected={selectedChannel}
           handleSelect={id => setSelectedChannel(id)}
-          updateSelectedPage={updateSelectedPage}
+          updateSelectedPage={updateSelectedPageAndMain}
           isCollapsed={isCollapsed}
           selectedPage="channels"
           handleCollapse={() => dispatch(toggleLeftPanel())}
@@ -43,9 +57,22 @@ export default function LeftPanelContainer() {
           friends={friends}
           selected={selectedChannel}
           handleSelect={id => setSelectedChannel(id)}
-          updateSelectedPage={updateSelectedPage}
+          updateSelectedPage={updateSelectedPageAndMain}
           isCollapsed={isCollapsed}
           selectedPage="friends"
+          handleCollapse={() => dispatch(toggleLeftPanel())}
+        />
+      </Route>
+      <Route exact path="/profile"></Route>
+      <Route>
+        <LeftPanel
+          channels={channels}
+          friends={friends}
+          selected={selectedChannel}
+          handleSelect={id => setSelectedChannel(id)}
+          updateSelectedPage={updateSelectedPanelPage}
+          isCollapsed={isCollapsed}
+          selectedPage={selectedPage}
           handleCollapse={() => dispatch(toggleLeftPanel())}
         />
       </Route>
