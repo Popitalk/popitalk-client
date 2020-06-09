253cbce04c1325a86737756f881b149d9dc02007
SKaralius <k.simonas.k@gmail.com>
2020-06-09 10:54:56 +0300

    Fixes a bug where chanel card icons would show through header popal modal.

--- src/comp/DropDowns/DropDownContainer.jsx
+++ src/comp/DropDowns/DropDownContainer.jsx
@@ -3,7 +3,7 @@ import classnames from "classnames";
 
 export default function DropDownContainer({ children }) {
   const classes = classnames(
-    "w-full relative sm:w-dropdown object-center bg-primaryBackground rounded-xl shadow-xl border border-highlightText overflow-hidden z-10"
+    "w-full relative sm:w-dropdown object-center bg-primaryBackground rounded-xl shadow-xl border border-highlightText overflow-hidden z-30"
   );
 
   return <div className={classes}>{children}</div>;
