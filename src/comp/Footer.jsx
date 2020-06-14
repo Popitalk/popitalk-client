1caa1442a902b2c6581acf24153722d70666c612
Andrew Jang <dhj9817@gmail.com>
2020-06-14 16:53:37 +0900

    footer linked up

--- src/comp/Footer.jsx
+++ src/comp/Footer.jsx
@@ -3,29 +3,62 @@ import React from "react";
 function Footer() {
   return (
     <footer className="w-full px-4 py-8 -mt-4 bg-gradient-r-primary text-tertiaryText sm:px-16">
-      <div className="grid grid-cols-2 row-gap-5 sm:flex sm:justify-start sm:space-x-16 sm:ml-10 sm:mt-5">
+      <div className="grid grid-cols-2 row-gap-6 sm:flex sm:justify-start sm:space-x-20 sm:ml-10 sm:mt-5">
         <div className="space-y-2 text-center">
           <span className="mb-4 text-lg font-bold">Company</span>
           <ul className="space-y-2 text-sm">
             <li>
-              <p>About Popitalk</p>
+              <a
+                className="text-tertiaryText no-underline"
+                href="https://about.popitalk.com/"
+              >
+                About Popitalk
+              </a>
             </li>
             <li>
-              <p>Jobs</p>
+              <a
+                className="text-tertiaryText no-underline"
+                href="https://medium.com/popitalk"
+              >
+                Blog
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-tertiaryText no-underline"
+                href="https://about.popitalk.com/"
+              >
+                Contact
+              </a>
             </li>
           </ul>
         </div>
         <div className="space-y-2 text-center">
           <span className="mb-4 text-lg font-bold">Legal</span>
-          <ul className="space-y-2 text-sm">
+          <ul className="space-y-2 text-sm text-tertiaryText no-underline">
             <li>
-              <p>Terms of Use</p>
+              <a
+                className="text-tertiaryText no-underline"
+                href="https://medium.com/popitalk/end-user-license-agreement-and-terms-of-service-dc8a25c0f5d2?source=friends_link&sk=2150df3f6e097d60599c30a5d3e8942a"
+              >
+                Terms of Use
+              </a>
             </li>
             <li>
-              <p>Privacy Policy</p>
+              <a
+                className="text-tertiaryText no-underline"
+                href="https://medium.com/popitalk/privacy-policy-ab89684edca6?source=friends_link&sk=555c056e49c784af16481f96f8dcbfe3"
+              >
+                Privacy Policy
+              </a>
             </li>
             <li>
-              <p>Copyright</p>
+              <a
+                className="text-tertiaryText no-underline"
+                href="https://medium.com/popitalk/copyright-policy-872f41dd7856?sk=38c4ab2d90555ed5ee6fe85373e06584"
+              >
+                Copyright
+              </a>
             </li>
           </ul>
         </div>
@@ -33,21 +66,28 @@ function Footer() {
           <span className="mb-4 text-lg font-bold">Community</span>
           <ul className="space-y-2 text-sm">
             <li>
-              <p>Guidelines</p>
+              <a
+                className="text-tertiaryText no-underline"
+                href="https://discord.gg/hdFfgg7"
+              >
+                Discord
+              </a>
             </li>
             <li>
-              <p>Support</p>
-            </li>
-          </ul>
-        </div>
-        <div className="space-y-2 text-center">
-          <span className="mb-4 text-lg font-bold">Business</span>
-          <ul className="space-y-2 text-sm">
-            <li>
-              <p>Branding</p>
+              <a
+                className="text-tertiaryText no-underline"
+                href="https://twitter.com/PopitalkT"
+              >
+                Twitter
+              </a>
             </li>
             <li>
-              <p>Contact</p>
+              <a
+                className="text-tertiaryText no-underline"
+                href="https://www.youtube.com/channel/UCJSjPolz6SiYKvVxFmK-Z1A"
+              >
+                Youtube
+              </a>
             </li>
           </ul>
         </div>
