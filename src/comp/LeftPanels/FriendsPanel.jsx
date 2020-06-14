32893b0da91fde2a8681ef7b6b6fb7cd748fb021
Andrew Jang <dhj9817@gmail.com>
2020-06-15 02:06:10 +0900

    fixed friends panel padding/added default channel asset

--- src/comp/LeftPanels/FriendsPanel.jsx
+++ src/comp/LeftPanels/FriendsPanel.jsx
@@ -46,65 +46,67 @@ export default function FriendsPanel({
   };
 
   return (
-    <div className="relative w-full h-full flex flex-col bg-primaryBackground xl:w-84 lg:w-84 md:w-84 sm:w-84">
+    <div className="w-full h-full flex flex-col bg-primaryBackground xl:w-84 lg:w-84 md:w-84 sm:w-84">
       <PanelHeader
         handleCollapse={handleCollapse}
         updateSelectedPage={updateSelectedPage}
         selectedPage={selectedPage}
       />
-      <div className="mt-2 mx-4">
-        <Input
-          variant="user"
-          size="sm"
-          value={search}
-          placeholder="Search with username"
-          onChange={e => setSearch(e.target.value)}
-          onClick={() => syncSearch(search)}
-        />
-      </div>
-      {open && (
-        <div className="rounded-lg bg-secondaryBackground shadow-inner mx-2 mt-2">
-          <div className="flex justify-between px-3 py-1 space-x-1">
-            <div>
-              <span className="text-xs">Results for </span>
-              <span className="text-xs font-bold">{search}</span>
-            </div>
-            <div>
-              <span
-                role="button"
-                className="text-xs font-semibold no-underline cursor-pointer text-highlightText"
-                onClick={() => syncSearch("")}
-              >
-                Close
-              </span>
-            </div>
-          </div>
-          <div className="flex w-full h-64">
-            <StretchList
-              list={FriendUsersList}
-              users={userSearchResults}
-              handleAccept={handleAccept}
-              handleReject={handleReject}
-              handleProfile={handleProfile}
-            />
-          </div>
+      <div className="">
+        <div className="mt-2 mx-4">
+          <Input
+            variant="user"
+            size="sm"
+            value={search}
+            placeholder="Search with username"
+            onChange={e => setSearch(e.target.value)}
+            onClick={() => syncSearch(search)}
+          />
         </div>
-      )}
-      <div className="bg-primaryBackground h-full">
-        <RoomsList
-          rooms={rooms}
-          selected={selectedRoom}
-          handleSelect={handleSelectRoom}
-          fullHeigth={true}
-        />
+        {open && (
+          <div className="rounded-lg bg-secondaryBackground shadow-inner mx-2 mt-2">
+            <div className="flex justify-between px-3 py-1 space-x-1">
+              <div>
+                <span className="text-xs">Results for </span>
+                <span className="text-xs font-bold">{search}</span>
+              </div>
+              <div>
+                <span
+                  role="button"
+                  className="text-xs font-semibold no-underline cursor-pointer text-highlightText"
+                  onClick={() => syncSearch("")}
+                >
+                  Close
+                </span>
+              </div>
+            </div>
+            <div className="flex w-full h-64">
+              <StretchList
+                list={FriendUsersList}
+                users={userSearchResults}
+                handleAccept={handleAccept}
+                handleReject={handleReject}
+                handleProfile={handleProfile}
+              />
+            </div>
+          </div>
+        )}
+        <div className="bg-primaryBackground h-full px-2 pt-2">
+          <RoomsList
+            rooms={rooms}
+            selected={selectedRoom}
+            handleSelect={handleSelectRoom}
+            fullHeigth={true}
+          />
+        </div>
+        <Button
+          size="md"
+          leftIcon="plus"
+          className="fixed bottom-0 left-0 ml-44 mb-4"
+        >
+          New Room
+        </Button>
       </div>
-      <Button
-        size="md"
-        leftIcon="plus"
-        className="fixed bottom-0 left-0 ml-44 mb-4"
-      >
-        New Room
-      </Button>
     </div>
   );
 }
