b82dc18d0a78ef16313b35bddceb1684b74ec776
SKaralius <k.simonas.k@gmail.com>
2020-06-18 14:51:30 +0300

    Adds a spinner to comp from compenents.

---
+++ src/comp/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Spinner.css";
+
+export default function Spinner() {
+  return (
+    <div className="ChatMessages--spinner">
+      <div className="ChatMessages--spinner--circle" />
+    </div>
+  );
+}
