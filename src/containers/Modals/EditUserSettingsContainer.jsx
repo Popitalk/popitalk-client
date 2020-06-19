bc3bec3da2d25ef326a57eebc4d43e83900cc88d
deluxe1cookie <romadryuckii@gmail.com>
2020-06-19 23:06:07 +0300

    result message after editing profile disappears when modal is closed

--- src/containers/Modals/EditUserSettingsContainer.jsx
+++ src/containers/Modals/EditUserSettingsContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { updateUser } from "../../redux/actions";
+import { clearError, updateUser } from "../../redux/actions";
 import ModalContainer from "../../comp/Modals/ModalContainer";
 import EditInformationModal from "../../comp/Modals/EditInformationModal";
 import ContainerHeader from "../../comp/ContainerHeader";
@@ -18,10 +18,15 @@ export default function EditUserSettingsModal({ handleModalClose }) {
   const updateUserApi = useSelector(state => state.api.userUpdateApi);
   const dispatch = useDispatch();
 
+  const closeModalAndClearError = () => {
+    handleModalClose();
+    dispatch(clearError());
+  };
+
   return (
     <ModalContainer
       isOpen={true}
-      handleModalClose={handleModalClose}
+      handleModalClose={closeModalAndClearError}
       header={<ContainerHeader title="Edit Your Information" />}
     >
       <EditInformationModal
