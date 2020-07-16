f3e57aa5591c49960d883697f94d832b6efb2587
Andrew Jang <dhj9817@gmail.com>
2020-07-16 23:48:06 +0900

    play/pause icon useState updated

--- src/comp/VideoPlayer.jsx
+++ src/comp/VideoPlayer.jsx
@@ -4,7 +4,13 @@ import ReactPlayer from "react-player";
 
 function VideoPlayer() {
   const [isHovering, setIsHovering] = useState(false);
+  const [playingIcon, playStatus] = useState(false);
   const [playing, handlePause] = useState(true);
+
+  const setBothPlaying = () => {
+    playStatus(!playingIcon);
+    handlePause(!playing);
+  };
 
   return (
     <>
@@ -26,19 +32,26 @@ function VideoPlayer() {
           <div className="flex flex-col justify-end w-full h-full transition-colors bg-gradient-t-player transition-opacity opacity-0 hover:opacity-100 duration-200">
             <div
               className="bg-transparent w-full h-full"
-              onClick={() => handlePause(!playing)}
+              onClick={() => setBothPlaying()}
               role="button"
-            />
+            >
+              <div className="flex h-full justify-center items-center">
+                <FontAwesomeIcon
+                  icon={playingIcon === true ? "play" : "pause"}
+                  className="text-tertiaryText"
+                />
+              </div>
+            </div>
             <div className="flex flex-col px-2">
               <button className="w-full h-1 rounded-xs bg-quaternaryBackground transition transform ease-in-out hover:scale-y-150 duration-100 focus:outline-none" />
               <div className="flex items-center justify-between w-full my-1">
                 <div className="space-x-4">
                   <button
                     className="w-8 p-1 rounded-full hover:bg-playerControlsHover focus:outline-none duration-50"
-                    onClick={() => handlePause(!playing)}
+                    onClick={() => setBothPlaying()}
                   >
                     <FontAwesomeIcon
-                      icon="play"
+                      icon={playingIcon === true ? "play" : "pause"}
                       className="text-tertiaryText"
                     />
                   </button>
