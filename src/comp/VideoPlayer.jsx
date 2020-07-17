12dfb689933d8799fb624aeb67388d1711221fda
Andrew Jang <dhj9817@gmail.com>
2020-07-18 04:37:39 +0900

    slider hover effect added

--- src/comp/VideoPlayer.jsx
+++ src/comp/VideoPlayer.jsx
@@ -5,6 +5,7 @@ import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 
 function VideoPlayer() {
+  const [isHovering, setIsHovering] = useState(false);
   const [playingIcon, playStatus] = useState(false);
   const [playing, handlePause] = useState(true);
   const [muted, handleMute] = useState(false);
@@ -46,23 +47,56 @@ function VideoPlayer() {
                 />
               </div> */}
             </button>
-            <div className="flex flex-col px-2 w-full">
+            <div
+              className="flex flex-col px-2 w-full"
+              onMouseEnter={() => setIsHovering(!isHovering)}
+              onMouseLeave={() => setIsHovering(!isHovering)}
+            >
               <Slider
-                trackStyle={{ backgroundColor: "#1DA4FE" }}
-                handleStyle={{
-                  backgroundColor: "#1DA4FE",
-                  borderColor: "#1DA4FE"
-                }}
-                railStyle={{
-                  backgroundColor: "#fff",
-                  borderColor: "#1DA4FE",
-                  opacity: 0.25
-                }}
+                handleStyle={
+                  isHovering === true
+                    ? {
+                        backgroundColor: "#1DA4FE",
+                        borderColor: "#1DA4FE",
+                        cursor: "pointer",
+                        width: 15,
+                        height: 15
+                      }
+                    : {
+                        width: 0,
+                        height: 0,
+                        border: 0
+                      }
+                }
+                trackStyle={
+                  isHovering === true
+                    ? {
+                        backgroundColor: "#1DA4FE",
+                        height: 6
+                      }
+                    : {
+                        backgroundColor: "#1DA4FE",
+                        height: 3
+                      }
+                }
+                railStyle={
+                  isHovering === true
+                    ? {
+                        backgroundColor: "#fff",
+                        opacity: 0.25,
+                        height: 6
+                      }
+                    : {
+                        backgroundColor: "#fff",
+                        opacity: 0.25,
+                        height: 3
+                      }
+                }
                 className="-mb-1 cursor-pointer transition-opacity opacity-75 hover:opacity-100 duration-150"
               ></Slider>
               {/* <button className="w-full h-1 rounded-xs bg-quaternaryBackground transition transform ease-in-out hover:scale-y-150 duration-100 focus:outline-none" /> */}
               <div className="flex items-center justify-between w-full my-1">
-                <div className="space-x-4">
+                <div className="flex space-x-4 items-center">
                   <button
                     className="w-8 p-1 rounded-full hover:bg-playerControlsHover focus:outline-none duration-100 transition transform ease-in-out hover:scale-110"
                     onClick={() => setBothPlaying()}
@@ -81,7 +115,7 @@ function VideoPlayer() {
                       className="text-tertiaryText"
                     />
                   </button>
-                  <span className="text-tertiaryText text-sm">0:11 / 5:04</span>
+                  <span className="text-tertiaryText text-xs">0:11 / 5:04</span>
                 </div>
                 <button className="w-8 p-1 rounded-full hover:bg-playerControlsHover focus:outline-none transition transform ease-in-out hover:scale-110 duration-100">
                   <FontAwesomeIcon
