d7dc2dc744b00b62233c210927105d119ef3c396
Andrew Jang <dhj9817@gmail.com>
2020-07-18 18:52:28 +0900

    collapse panel changed collapse

--- src/comp/LeftPanels/CollapsedPanel.jsx
+++ src/comp/LeftPanels/CollapsedPanel.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 import RoomIcon from "../Controls/RoomIcon";
 import Button from "../Controls/Button";
-import useCollapse from "react-collapsed";
+import { Collapse } from "react-collapse";
 
 function CollapsedPanel({
   rooms,
@@ -12,16 +12,16 @@ function CollapsedPanel({
   handleSelectRoom,
   handleCollapse
 }) {
-  const { getCollapseProps, getToggleProps } = useCollapse();
+  const [channelSelected, setChannelSelected] = useState(true);
+  const [friendSelected, setFriendSelected] = useState(true);
 
-  const [chipSelected, setChipSelected] = useState("");
-  const onChipClick = title => {
-    if (chipSelected === title) {
-      setChipSelected("");
-    } else {
-      setChipSelected(title);
-    }
+  const onChannelClick = () => {
+    setChannelSelected(!channelSelected);
   };
+  const onFriendClick = () => {
+    setFriendSelected(!friendSelected);
+  };
+
   return (
     <div className="bg-primaryBackground px-2 flex flex-col items-center w-20 h-full select-none">
       <button
@@ -39,27 +39,13 @@ function CollapsedPanel({
           className="flex flex-col h-12 w-20 bg-secondaryBackground mb-1 shadow-none"
           shape="none"
           background="bgColor"
-          selectedColor={chipSelected === "following" && "primary"}
+          selectedColor={channelSelected === true && "primary"}
           size="sm"
-          {...getToggleProps({
-            onClick: () => onChipClick("following")
-          })}
+          onClick={onChannelClick}
         >
           Channels
         </Button>
-        {/* CHANNEL BUTTON WITH ICON */}
-        {/* <button
-          className={`${
-            selectedPage === "channels"
-              ? "text-highlightText"
-              : "text-secondaryButtonText"
-          } py-4 w-full flex items-center flex-col hover:text-highlightText duration-100 rounded-xl focus:outline-none transition transform ease-in-out hover:scale-110 duration-100`}
-          onClick={() => updateSelectedPage("channels")}
-        >
-          <FontAwesomeIcon icon="tv" className="fa-1x" />
-          <h3 className="text-xs p-1">Channels</h3>
-        </button> */}
-        <section {...getCollapseProps()}>
+        <Collapse isOpened={channelSelected}>
           <div className="flex flex-col w-full items-center">
             {channels.map(channel => {
               const roomIcon = (
@@ -84,7 +70,7 @@ function CollapsedPanel({
               );
             })}
           </div>
-        </section>
+        </Collapse>
       </div>
       {/* FRIENDS */}
       <div className="bg-primaryBackground rounded-xl mb-4">
@@ -92,27 +78,13 @@ function CollapsedPanel({
           className="flex flex-col h-12 w-20 mb-1 bg-secondaryBackground shadow-none"
           shape="none"
           background="bgColor"
-          selectedColor={chipSelected === "discover" && "primary"}
-          onClick={() => onChipClick("discover")}
+          selectedColor={friendSelected === true && "primary"}
+          onClick={() => onFriendClick()}
           size="sm"
-          // {...getToggleProps({
-          //   onClick: () => onChipClick("discover")
-          // })}
         >
           Friends
         </Button>
-        {/* <button
-          className={`${
-            selectedPage === "friends"
-              ? "text-highlightText"
-              : "text-secondaryButtonText"
-          } py-4 w-full flex items-center flex-col hover:text-highlightText duration-100 rounded-xl focus:outline-none transition transform ease-in-out hover:scale-110 duration-100`}
-          onClick={() => updateSelectedPage("friends")}
-        >
-          <FontAwesomeIcon icon="user-friends" className="fa-1x" />
-          <h3 className="text-xs p-1">Friends</h3>
-        </button> */}
-        <section>
+        <Collapse isOpened={friendSelected}>
           <div className="flex flex-col w-full items-center">
             {rooms.map(room => {
               const images = room.members.map(m => m.avatar);
@@ -141,7 +113,7 @@ function CollapsedPanel({
               );
             })}
           </div>
-        </section>
+        </Collapse>
       </div>
     </div>
   );
