c5b90267921853e597738f6b9abf17365e06e2c6
SKaralius <k.simonas.k@gmail.com>
2020-08-28 00:51:59 +0300

    Gif state expands instead of replacing.

--- src/redux/reducers/gifs.js
+++ src/redux/reducers/gifs.js
@@ -4,7 +4,9 @@ import { getTrendingGifs } from "../actions";
 const initialState = {};
 
 const R_getTrendingGifs = (state, { payload }) => {
-  state.trending = payload;
+  if (!state.trending) {
+    state.trending = payload;
+  } else state.trending = [...state.trending, ...payload];
 };
 
 export default createReducer(initialState, {
