64dd55632145f4b36970c9e7e3c57d1d3bce48e7
SKaralius <k.simonas.k@gmail.com>
2020-09-03 18:35:40 +0300

    Messages in channels no longer add a notif in chat.

--- src/util/channelHasNewMessage.js
+++ src/util/channelHasNewMessage.js
@@ -1,5 +1,9 @@
 // Taskes a channel or a room
 export function channelHasNewMessage(channel) {
-  if (channel.chatNotifications || channel.lastMessageIsNew) return true;
+  if (
+    (channel.type === "friend" || channel.type === "group") &&
+    (channel.chatNotifications || channel.lastMessageIsNew)
+  )
+    return true;
   else return false;
 }
