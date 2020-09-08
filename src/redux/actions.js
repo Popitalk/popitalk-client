c702deb59853129e08858ecef2c70023674e14ac
sulaiman1073 <sulaimanNajeebAlabbar@gmail.com>
2020-09-08 14:56:35 +0300

    aws test 1

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -39,6 +39,7 @@ export const validateSession = createAsyncThunk(
   "general/validateSession",
   async () => {
     const response = await api.validateSession();
+    console.log("test", response);
     return response.data;
   }
 );
