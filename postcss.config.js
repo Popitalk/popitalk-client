1d070e17a06c67e7d56837d675bf9e3cf110ff38
sulaiman1073 <sulaimanNajeebAlabbar@gmail.com>
2020-09-11 12:50:15 +0300

    postcss, changed default extractor

--- postcss.config.js
+++ postcss.config.js
@@ -4,12 +4,12 @@ const postcssNesting = require("postcss-nesting");
 const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./src/**/*.jsx", "./public/index.html"],
 
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-  // defaultExtractor: content => {
-  //   const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-  //   const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-  //   return broadMatches.concat(innerMatches);
-  // }
+  // defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: content => {
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+    return broadMatches.concat(innerMatches);
+  }
 });
 
 module.exports = {
