222b159d1e8c5cbc0f60900f1807821647a2df8b
sulaiman1073 <sulaimanNajeebAlabbar@gmail.com>
2020-09-11 05:12:12 +0300

    css test

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,11 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./src/**/*.jsx", "./public/index.html"],
 
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  // defaultExtractor: content => {
+  //   const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+  //   const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+  //   return broadMatches.concat(innerMatches);
+  // }
 });
 
 module.exports = {
