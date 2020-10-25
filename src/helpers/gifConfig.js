40cb2ac71599ca7b3e220f632621393a48d7d118
Andrew Jang <dhj9817@gmail.com>
2020-10-25 22:49:55 +0900

    Added the GIPHY API key in the client's code

--- src/helpers/gifConfig.js
+++ src/helpers/gifConfig.js
@@ -1,5 +1,5 @@
 import { GiphyFetch } from "@giphy/js-fetch-api";
 
-const giphyFetch = new GiphyFetch(process.env.REACT_APP_GIPHY_API_KEY);
+const giphyFetch = new GiphyFetch("rOIEWduHOAJEEnQgtx7Jp6nuyi0nX5W4");
 
 export default giphyFetch;
