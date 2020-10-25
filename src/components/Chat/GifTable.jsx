7d71ec5e031261cfd9986958a6e3e8e686c7f9d0
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-10-25 20:44:12 +0300

    Revert "Resolving conflicts"

--- src/components/Chat/GifTable.jsx
+++ src/components/Chat/GifTable.jsx
@@ -1,24 +1,14 @@
 import React, { useState } from "react";
 import Input from "../Controls/Input";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  addMessage,
-  getTrendingGifs,
-  getSearchGifs,
-  saveOffset,
-  setDisplay
-} from "../../redux/actions";
 import { useParams } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import strings from "../../helpers/localization";
-import { gifsDisplay } from "../../helpers/constants";
-import Button from "../Controls/Button";
+import { Grid } from "@giphy/react-components";
+import giphyFetch from "../../helpers/gifConfig";
+import { useSelector, useDispatch } from "react-redux";
+import { addMessage } from "../../redux/actions";
 
 export default function GifTable({ updateGifsOpen }) {
-  const offset = useSelector(state => state.gifs.offset);
-  const display = useSelector(state => state.gifs.display);
-  const trendingGifs = useSelector(state => state.gifs.trending);
-  const searchResultGifs = useSelector(state => state.gifs.searchResults);
   const params = useParams();
   const channelId = params.channelId || params.roomId;
   const userId = useSelector(state => state.self.id);
@@ -32,11 +22,7 @@ export default function GifTable({ updateGifsOpen }) {
         id: uuidv4(),
         userId,
         channelId,
-        content: JSON.stringify({
-          id: gif.id,
-          title: gif.title,
-          image: gif.image
-        }),
+        content: gif.id,
         upload: "gif",
         createdAt: new Date().toString(),
         author: {
@@ -48,29 +34,11 @@ export default function GifTable({ updateGifsOpen }) {
     );
     // updateGifsOpen(false);
   };
-  function displayGifs() {
-    let gifs = trendingGifs;
-    if (display === gifsDisplay.searchResults) {
-      gifs = searchResultGifs;
-    }
 
-    return gifs.map(gif => {
-      return (
-        <li
-          key={gif.id}
-          role="button"
-          onClick={() => handleSendGif(gif)}
-          className="relative hover:bg-highlightText rounded-sm duration-75"
-        >
-          <img
-            className="w-full max-h-none p-1 rounded-md"
-            src={gif.image}
-            alt={gif.title}
-          />
-        </li>
-      );
-    });
-  }
+  const gifsPromise = search
+    ? offset => giphyFetch.search(search, { offset, limit: 10 })
+    : offset => giphyFetch.trending({ offset, limit: 10 });
+
   return (
     <div className="flex flex-col giftable relative bg-secondaryBackground rounded-md w-84 h-64">
       <Input
@@ -79,49 +47,20 @@ export default function GifTable({ updateGifsOpen }) {
         value={search}
         placeholder={strings.searchGifInput}
         onChange={e => setSearch(e.target.value)}
-        onClick={() => {
-          dispatch(setDisplay(gifsDisplay.searchResults));
-          dispatch(getSearchGifs({ term: search, offset: 0 }));
-        }}
-        onKeyDown={e => {
-          if (e.key === "Enter") {
-            dispatch(setDisplay(gifsDisplay.searchResults));
-            dispatch(getSearchGifs({ term: search, offset: 0 }));
-          }
-        }}
         className="p-1"
       />
-      <div className="overflow-y-auto">
-        <ul className="flex items-center grid grid-cols-2 overflow-y-auto py-4">
-          {displayGifs()}
+      <div className="overflow-y-auto" style={{ minHeight: "214px" }}>
+        <ul className="items-center grid grid-cols-2 overflow-y-auto py-4">
+          <Grid
+            key={search}
+            columns={2}
+            width={330}
+            fetchGifs={gifsPromise}
+            noLink
+            className="cursor-pointer"
+            onGifClick={handleSendGif}
+          />
         </ul>
-        <div className="flex justify-center w-full">
-          {display === gifsDisplay.trending ? (
-            <Button
-              styleNone
-              styleNoneContent={strings.loadMoreButton}
-              hoverable
-              styleNoneContentClassName="text-highlightText text-xs font-bold inline-block px-4 py-2"
-              className="hover:bg-highlightBackground rounded-xl my-2"
-              onClick={() => {
-                const newOffset = offset + 10;
-                dispatch(getTrendingGifs(newOffset));
-                dispatch(saveOffset(newOffset));
-              }}
-            />
-          ) : (
-            <Button
-              styleNone
-              styleNoneContent={strings.backToTrendingButton}
-              hoverable
-              styleNoneContentClassName="text-highlightText text-xs font-bold inline-block px-4 py-2"
-              className="hover:bg-highlightBackground my-2 rounded-xl"
-              onClick={() => {
-                dispatch(setDisplay(gifsDisplay.trending));
-              }}
-            />
-          )}
-        </div>
       </div>
     </div>
   );
