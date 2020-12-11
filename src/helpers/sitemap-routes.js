30d523781612d676311735ed40a92b03acd33b27
Andrew Jang <dhj9817@gmail.com>
2020-12-11 23:19:11 +0900

    Added static sitemap to solve the crawl issue with SPA

---
+++ src/helpers/sitemap-routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Route from "react-router";
+import { Redirect } from "react-router-dom";
+import { PublicRoute, GeneralRoute, PrivateRoute } from "../components/Routers";
+
+export default (
+  <Route>
+    <PublicRoute exact path="/welcome" />
+    <GeneralRoute exact path="/" />
+    <GeneralRoute exact path="/channels/:channelId/:tab" />
+    <GeneralRoute exact path="/friends" />
+    <PrivateRoute exact path="/create" />
+    <PrivateRoute exact path="/rooms/:roomId/video" />
+    <PrivateRoute exact path="/users/:userId" />
+    <Route path="*">
+      <Redirect to="/" />
+    </Route>
+  </Route>
+);
