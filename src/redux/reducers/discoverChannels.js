7cc1597a8e17e9e2f1ceeb3437bc5c50b2394f36
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-12-24 22:14:54 +0200

    updated trending/discover/following reducers to paginate channels list with a load more button

--- src/redux/reducers/discoverChannels.js
+++ src/redux/reducers/discoverChannels.js
@@ -4,13 +4,25 @@ import { logout, deleteAccount, getDiscoverChannels } from "../actions";
 const initialState = {
   channels: {},
   users: {},
+  page: 1,
+  isNextPage: false,
   lastRequestAt: null
 };
 
 const R_addDiscoverChannels = (state, { payload }) => {
-  state.channels = payload.channels;
-  state.users = payload.users;
+  const channelsCount = Object.keys(payload.channels).length;
+
+  state.page += 1;
+  state.isNextPage = channelsCount === 30;
   state.lastRequestAt = new Date().toString();
+
+  if (payload.page === 1) {
+    state.users = payload.users;
+    state.channels = payload.channels;
+  } else {
+    state.users = { ...state.users, ...payload.users };
+    state.channels = { ...state.channels, ...payload.channels };
+  }
 };
 
 const R_resetState = () => initialState;
