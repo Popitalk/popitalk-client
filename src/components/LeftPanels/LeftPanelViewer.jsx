9d56ef6ac2e75a03f14525de75cf5d93638e22e4
Andrew Jang <dhj9817@gmail.com>
2021-01-02 03:30:10 +0900

    Responsive fix2 for tablets

--- src/components/LeftPanels/LeftPanelViewer.jsx
+++ src/components/LeftPanels/LeftPanelViewer.jsx
@@ -13,7 +13,7 @@ export default function LeftPanelViewer({
   handleSelectChannel
 }) {
   return (
-    <div className="flex flex-col w-screen md:w-84 h-full bg-background-primary select-none">
+    <div className="flex flex-col w-screen sm:w-84 h-full bg-background-primary select-none">
       <div className="flex flex-col justify-between h-full px-2 space-y-2">
         <div className="bg-background-secondary px-2 py-4 shadow-md rounded-md ">
           <InviteForm link={link} />
